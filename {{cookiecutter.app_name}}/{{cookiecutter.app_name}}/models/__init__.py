32cd8d34af9b94c27b5ee5795171f85033a4d2db
Avelino <thiago@avelino.xxx>
2015-11-11 15:54:49 -0200

    clean cookiecutter startproject, migrate to sample command

--- {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/models/__init__.py
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/models/__init__.py
@@ -1,5 +1,1 @@
 # -*- coding: utf-8 -*-
-from sqlalchemy import create_engine
-
-
-engine = create_engine('sqlite:///:memory:', echo=True)
--- {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/routes.py
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/routes.py
@@ -2,11 +2,8 @@
 from bottle import Bottle
 
 from .controllers.home import home_app
-from .controllers.user import user_app
 
 
 Routes = Bottle()
 # App to render / (home)
 Routes.merge(home_app)
-# Mount other applications
-Routes.mount("/user", user_app)
